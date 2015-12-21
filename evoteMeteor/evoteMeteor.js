2e8ead29fb5a4cdd50fb14b085e7cf643c5f78be
blankon123 <thosan.girisona@gmail.com>
2015-12-21 22:15:48 +0700

    Installing Meteor + Move Files + Migrate(little)

--- evoteMeteor/evoteMeteor.js
+++ evoteMeteor/evoteMeteor.js
@@ -1,17 +1,29 @@
+LoginLog = new Mongo.Collection('LoginLog');
+User = new Mongo.Collection('Users');
+VotingResult = new Mongo.Collection('VotingResult');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .form-horizontal": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var nim = event.target.nim.value;
+      var ada = User.findOne(nim);
+      
+      // Insert a task into the collection
+      if (!ada) {
+        LoginLog.insert({
+          user: nim,
+          loginAt: new Date() // current time
+        });
+        Session.set('nim', nim);
+      };
+ 
+      // Clear form
+      event.target.nim.value = "";
+      alert(nim);
     }
   });
 }
