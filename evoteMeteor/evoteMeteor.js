ca11d1f737c3268cf98ae6b9b02c21db0c661e59
blankon123 <thosan.girisona@gmail.com>
2015-12-22 02:21:16 +0700

    Templating

--- evoteMeteor/evoteMeteor.js
+++ evoteMeteor/evoteMeteor.js
@@ -29,7 +29,10 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.startup({
+    'sendLogMessage': function(){
+        Session.setDefault('sudahVoting', false);
+        Session.setDefault('sudahLogin', false);
+    }
   });
 }
