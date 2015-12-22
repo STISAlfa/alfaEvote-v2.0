53f7c22fa64210067020a4cc578560da9a30fc6d
blankon123 <thosan.girisona@gmail.com>
2015-12-22 21:46:26 +0700

    Templating+add iron:router

--- evoteMeteor/evoteMeteor.js
+++ evoteMeteor/evoteMeteor.js
@@ -1,38 +1,54 @@
 LoginLog = new Mongo.Collection('LoginLog');
 User = new Mongo.Collection('Users');
 VotingResult = new Mongo.Collection('VotingResult');
+pw = '';
 
 if (Meteor.isClient) {
+  Session.setDefault('sudahVoting', false);
+  Session.setDefault('sudahLogin', false);
+
   Template.body.events({
     "submit .form-horizontal": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
  
       // Get value from form element
-      var nim = event.target.nim.value;
-      var ada = User.findOne(nim);
+      pw = event.target.pw.value;
+      var ada = User.findOne(pw);
       
       // Insert a task into the collection
-      if (!ada) {
+      if (ada) {
         LoginLog.insert({
-          user: nim,
+          user: pw,
           loginAt: new Date() // current time
         });
-        Session.set('nim', nim);
-      };
- 
-      // Clear form
-      event.target.nim.value = "";
-      alert(nim);
-    }
+        Session.set('user', pw);}
+      else{
+        User.insert({
+          user: pw,
+          createAt: new Date() // current time
+        });};
+      event.target.pw.value = "";
+      Meteor.go('voting');}
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup({
-    'sendLogMessage': function(){
-        Session.setDefault('sudahVoting', false);
-        Session.setDefault('sudahLogin', false);
-    }
+if (Meteor.isServer){
+  Meteor.startup(function (){
+
   });
+
+  Router.route('/voting', function () {
+     if (Session.get('user')) {
+       var user = User.find({pw: Session.get('user')});
+       this.render('voting', {data: user});
+     }
+     else {
+       Meteor.go('/');
+     }});
+
+  Router.route('/', function () {
+     Session.setDefault('sudahVoting', false);
+     Session.setDefault('sudahLogin', false);
+     this.render('login');});
 }
