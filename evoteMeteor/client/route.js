2bc9a2121a20280c2c365b201313550bbbba2b39
blankon123 <thosan.girisona@gmail.com>
2016-01-07 16:48:58 +0700

    Flow router still bugging, mongodb running, submit button not responding

---
+++ evoteMeteor/client/route.js
@@ -0,0 +1,24 @@
+FlowRouter.route('/', {
+  action: function() {
+    Session.setDefault('sudahVoting', false);
+    Session.setDefault('sudahLogin', false);
+    BlazeLayout.render("Main", {
+      header: "header",
+      content: "login",
+      footer: "footer"});
+  }
+});
+
+FlowRouter.route('/voting', function () {
+     if (Session.get('user')) {
+       var sema     = CalonSema.find();
+       var angkatan = CalonAngkatan.find({angkatan: Session.get('user')[0]});
+       BlazeLayout.render("Main", {
+          header: "header",
+          content: "voting",
+          footer: "footer"});
+     }
+     else {
+       Route.go('/');
+     }
+});
---
+++ evoteMeteor/collection.js
@@ -0,0 +1,5 @@
+LoginLog      = new Mongo.Collection('LoginLog');
+User          = new Mongo.Collection('Users');
+VotingResult  = new Mongo.Collection('VotingResult');
+CalonSema     = new Mongo.Collection('CalonSema');
+CalonAngkatan = new Mongo.Collection('CalonAngkatan');
